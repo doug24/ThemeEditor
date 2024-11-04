5ab5dc605b026de191e921e15501d71865166bb3
doug24 <17227248+doug24@users.noreply.github.com>
2024-11-04 14:56:59 -0800

    Add nlog and unhandled exception handler

--- ThemeEditor/App.xaml.cs
+++ ThemeEditor/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using NLog;
 
 namespace ThemeEditor
 {
@@ -7,12 +10,54 @@ namespace ThemeEditor
     /// </summary>
     public partial class App : Application
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            logger.Info("Theme Editor started");
+            SetupExceptionHandling();
+
             MainWindow = new MainWindow();
             MainWindow.Show();
         }
 
         public ResourceDictionary ThemeResources => Resources.MergedDictionaries[0];
+
+
+        private void SetupExceptionHandling()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+                LogUnhandledException((Exception)e.ExceptionObject, "AppDomain.CurrentDomain.UnhandledException");
+
+            DispatcherUnhandledException += (s, e) =>
+            {
+                LogUnhandledException(e.Exception, "Application.Current.DispatcherUnhandledException");
+                e.Handled = true;
+            };
+
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                LogUnhandledException(e.Exception, "TaskScheduler.UnobservedTaskException");
+                e.SetObserved();
+            };
+        }
+
+        private void LogUnhandledException(Exception exception, string source)
+        {
+            string message = $"Unhandled exception ({source})";
+            try
+            {
+                System.Reflection.AssemblyName assemblyName = System.Reflection.Assembly.GetExecutingAssembly().GetName();
+                message = string.Format("Unhandled exception in {0} v{1}", assemblyName.Name, assemblyName.Version);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Exception in LogUnhandledException");
+            }
+            finally
+            {
+                logger.Error(exception, message);
+            }
+        }
     }
 }
